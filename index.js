7a0568d5c76b509bb0e89d5cfc1d0330960f7df9
AnissaLopez <anissa965@gmail.com>
2023-03-21 16:15:58 -0400

    maths is hard

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b){
+    return a += b;
+
+}
+function subtract(a,b){
+    return a -= b;
+
+}
+function multiply(a,b){
+    return a *= b
+
+}
+function divide(a,b){
+    return a /= b;
+
+}
+function makeInt(string) {
+    let result = parseInt(string, 10);
+    return result;
+}
+function preserveDecimal(string){
+    let result = parseFloat(string);
+    return result;
+}
+function increment(n){
+    return n+=1
+}
+function decrement(n){
+    return n-=1
+}
